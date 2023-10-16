aee59830ef6d62d994386163b315db307c76e00f
Razmo <you@example.com>
2023-10-16 10:17:09 +0200

    ex12

---
+++ ex_12/ex_12.sql
@@ -0,0 +1,1 @@
+SELECT COUNT(title) AS "Number of \'western\' movies" FROM movies INNER JOIN genres ON movies.genre_id = genres.id INNER JOIN producers ON movies.producer_id = producers.id WHERE genres.name = "western" AND (producers.name = "tartan movies" OR producers.name = "lionsgate uk");
